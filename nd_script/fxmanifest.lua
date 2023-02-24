c62ce4542e5ff83c33e328fcaedae001a1b25626
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-02-24 03:06:50 +0100

    Add files via upload

---
+++ nd_script/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+
+--Additional files here
+files {
+}
+
+--Client sided scripts
+client_scripts {
+  'client/*.lua',
+}
+
+--Both server and client sided scripts
+shared_script {
+  'shared/*.*',
+}
+
+--Server sided scripts
+server_scripts {
+  'server/*.lua',
+}
